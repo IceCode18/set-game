9cde23b606e257d9c5a8fad36d82ef8139bc3873
Jtiano <jh.tiano@gmail.com>
2023-03-17 21:48:27 -1000

    create card view

---
+++ SetGame/CardView.swift
@@ -0,0 +1,21 @@
+//
+//  CardView.swift
+//  SetGame
+//
+//  Created by Kaiser19 on 6/2/19.
+//  Copyright © 2019 Kaiser19. All rights reserved.
+//
+
+import UIKit
+
+class CardView: UIView {
+
+    /
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    
+
+}
